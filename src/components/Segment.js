b3e339d8ba2773269f753ee1fda213cb08528f67
rkr-abs <rkr@abstractit.in>
2023-03-20 18:35:15 +0530

    Add Rural and urban percentage

--- src/components/Segment.js
+++ src/components/Segment.js
@@ -1,16 +1,35 @@
+/* eslint-disable max-lines-per-function */
 import React from 'react';
 
 const Segment = (context) => {
-	const { data: { population }} = context;
+	const {
+		data: { population },
+	} = context;
 
-	return <span>
-		{population.state}
-		<div
-			className="container-state"
-			style={ {
-				width: `${ population.percentage }%`,
-			} }
-		/></span>;
+	return (
+		<span>
+			{population.state}
+			<div
+				className="container-state"
+				style={ {
+					width: `${ population.percentage }%`,
+				} }
+			>
+				<div
+					className="container-rural"
+					style={ {
+						width: `${ population.ruralPercent }%`,
+					} }
+				/>
+				<div
+					className="container-urban"
+					style={ {
+						width: `${ population.urbanPercent }%`,
+					} }
+				/>
+			</div>
+		</span>
+	);
 };
 
 export default Segment;
